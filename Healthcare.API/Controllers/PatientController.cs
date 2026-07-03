b19d58569f715bf05de28a26b4ff2ee137c251fa
Saurabh Bhimrao Dhas <saurabhdhas15355@gmail.com>
2026-07-03 16:22:26 +0530

    Update PatientController.cs
    Uodate code

--- Healthcare.API/Controllers/PatientController.cs
+++ Healthcare.API/Controllers/PatientController.cs
@@ -28,7 +28,7 @@ namespace Healthcare.API.Controllers
             return Ok(new ApiResponse<object>
             {
                 Success = true,
-                Message = "Patients fetched successfully",
+                Message = "Patients retrive successfully",
                 Data = patients
             });
         }
